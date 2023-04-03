3a00dfaac4ec78d38d45c1e5c2e3dd3ff641a66e
EmersonHdz <emersonhdz94@gmail.com>
2023-04-03 13:15:00 +0100

    update composer

--- resources/views/components/forms/alert.blade.php
+++ resources/views/components/forms/alert.blade.php
@@ -1,4 +1,4 @@
 <div class="bg-green-100 border border-green-400 text-green-700 px-4 py-3 mt-5 rounded relative" role="alert">
-    <strong class="font-bold">Success!!!</strong>
+    <strong class="font-bold">Success!</strong>
     <span class="block sm:inline">{{ session('status') }}</span>
     </div>
